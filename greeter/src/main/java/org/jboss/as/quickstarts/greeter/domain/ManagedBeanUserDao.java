d590666793fe8982fac1bf24e73c05375dc4072f
Dominik Obermaier <dominik.obermaier@dc-square.de>
2012-06-11 22:17:15 +0200

    added transactions with annotations

--- greeter/src/main/java/org/jboss/as/quickstarts/greeter/domain/ManagedBeanUserDao.java
+++ greeter/src/main/java/org/jboss/as/quickstarts/greeter/domain/ManagedBeanUserDao.java
@@ -16,60 +16,37 @@
  */
 package org.jboss.as.quickstarts.greeter.domain;
 
+
+import pl.softwaremill.common.cdi.transaction.Transactional;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
-import javax.transaction.SystemException;
 import javax.transaction.UserTransaction;
 
 public class ManagedBeanUserDao implements UserDao {
 
-   @Inject
-   private EntityManager entityManager;
+    @Inject
+    private EntityManager entityManager;
 
-   @Inject
-   private UserTransaction utx;
+    @Inject
+    private UserTransaction utx;
 
-   public User getForUsername(String username) {
-      try {
-         try {
-            utx.begin();
-            try {
-               Query query = entityManager.createQuery("select u from User u where u.username = :username");
-               query.setParameter("username", username);
-               return (User) query.getSingleResult();
-            } catch (NoResultException e) {
-               return null;
-            }
-         } finally {
-            utx.commit();
-         }
-      } catch (Exception e) {
-         try {
-            utx.rollback();
-         } catch (SystemException se) {
-            throw new RuntimeException(se);
-         }
-         throw new RuntimeException(e);
-      }
-   }
+    @Transactional
+    public User getForUsername(String username) {
+        try {
+            Query query = entityManager.createQuery("select u from User u where u.username = :username");
+            query.setParameter("username", username);
+            return (User) query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 
-   public void createUser(User user) {
-      try {
-         try {
-            utx.begin();
-            entityManager.persist(user);
-         } finally {
-            utx.commit();
-         }
-      } catch (Exception e) {
-         try {
-            utx.rollback();
-         } catch (SystemException se) {
-            throw new RuntimeException(se);
-         }
-         throw new RuntimeException(e);
-      }
-   }
+    @Transactional
+    public void createUser(User user) {
+        entityManager.persist(user);
+
+    }
 }
